cf33af28515ecc842333b4692f5d23671e617596
vaxtalastus-web <vaxtalastus@gmail.com>
2025-11-13 19:51:31 +1100

    Update cd.lua

--- src/cd.lua
+++ src/cd.lua
@@ -1,6 +1,1 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "pshade", 
-	Text = "please wait", 
-	Icon = ""
-})
-loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/pshade-ultimate/refs/heads/main/src/back.json"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/pshade-ultimate/refs/heads/main/src/back.lua"))()
